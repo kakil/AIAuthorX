81823aa8c2c6c969d1f6d7f5b6cf9d958b3c2cdf
Kitwana Akil <kakil@me.com>
2023-04-19 02:40:03 -0400

    Updated to add config file to the release.

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+$sitename='My Prompt SaaS';
+$logo='img/logo.png';
+$runbutton=true; 
+$masterkeymode=false;
+$masterapikey='';
+$logoutredirect='login.php';
+$adminuser='admin';
+$adminpassword='WMT2fjq3pax-yde-xge';
+$footer=<<<EOTFOOTER
+<br><br><br><hr><div style="color:#777; font:size:14px; display:block; float:left;">(C)2023 My Prompt SaaS - For terms click <a href="terms.php" target="_BLANK">HERE</a></div><div style="color:#777; float:right">Click <a href="https://supportlink.com">HERE</a> for support</div><br><hr>
+EOTFOOTER;
+
+define('USER_DB_HOST', 'localhost');
+define('USER_DB_NAME', 'takeacti_myPromptSaas');
+define('USER_DB_CHARSET', 'utf8mb4');
+define('USER_DB_USER', 'takeacti_myPromptSaaSAdmin');
+define('USER_DB_PASSWORD', 'WMT2fjq3pax-yde-xge');
+?>
